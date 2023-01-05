1ecc1c33dcef3b813a3549a36b52c545f9ba8e3c
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2023-01-05 18:14:33 -0300

    Update exceptions.js

--- facpack_behavior/scripts/system/exceptions.js
+++ facpack_behavior/scripts/system/exceptions.js
@@ -1,11 +1,15 @@
+class OtherExceptions {
+    static InvalidArgument = "the value of the argument is invalid";
+}
+
 class FactionHandlerException {
     static PlayerHasNotFaction = "the player does not have a faction";
 
     static NotMemberError = "the player is not member member of this faction";
 
-    static CantCreateExistingFaction  = "you're trying to create a existing faction";
+    static CantCreateExistingFaction = "you're trying to create an existing faction";
 
     static InvalidFactionId = "invalid faction id";
 }
 
-export { FactionHandlerException };
+export { OtherExceptions, FactionHandlerException };
