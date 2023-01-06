51da57f3a65bbf6217df27ac4231ff4a1b141140
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2023-01-06 13:52:14 -0300

    Create Adivices File
    This file contains the advice texts of diferent languagens to be exported.

---
+++ facpack_behavior/scripts/conf/advices.texts.js
@@ -0,0 +1,20 @@
+export const pt_br = {
+    fac: {
+        create_success: 'Criado com sucesso!',
+        create_fail: {
+            id_fail: 'Não foi possível criar sua facção. Tente novamente, lembre-se que o nome da facção precisa ter de 3 a 16 caracteres e só pode conter letras, números e espaços.',
+            exists_fail: 'A facção que você está tentando criar já existe.',
+            has_faction_fail: 'Você atualmente já participa de uma facção.'
+        },
+        delete_success: 'Facção apagada com sucesso.',
+        delete_fail: {
+            uncaught_fail: 'Não foi possível deletar a facção.',
+            has_not_faction: 'Você não têm uma facção.',
+            not_leader_fail: 'Você não é o lider da facção qual está tentando deletar'
+        }
+    }
+}
+
+export const en_us = {
+
+}
