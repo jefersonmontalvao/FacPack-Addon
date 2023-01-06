e4a6b2174a929f14d81758bce7039f698ea5fe4a
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2023-01-06 17:49:11 -0300

    Revision/Correction + Comments

--- facpack_behavior/scripts/modules/mc_world.utils.js
+++ facpack_behavior/scripts/modules/mc_world.utils.js
@@ -1,15 +1,25 @@
-import { world, MinecraftDimensionTypes } from '@minecraft/server'
+import { world, MinecraftDimensionTypes } from '@minecraft/server';
 
+/**
+ * Run a command using overworld object.
+ * The overworld starts at coordinate 0 0 0.
+ */
 function runMCCommandAtOverworld(cmd) {
     return world.getDimension(MinecraftDimensionTypes.overworld).runCommandAsync(cmd);
 }
 
+/**
+ * Run a command by nether dimesion.
+ */
 function runMCCommandAtNether(cmd) {
     return world.getDimension(MinecraftDimensionTypes.nether).runCommandAsync(cmd);
 }
 
+/**
+ * Run a command by theend dimension.
+ */
 function runMCCommandAtEnd(cmd) {
     return world.getDimension(MinecraftDimensionTypes.theEnd).runCommandAsync(cmd);
 }
 
-export {runMCCommandAtOverworld, runMCCommandAtNether, runMCCommandAtEnd}
+export {runMCCommandAtOverworld, runMCCommandAtNether, runMCCommandAtEnd};
