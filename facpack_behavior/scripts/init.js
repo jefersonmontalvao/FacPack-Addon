042e8d6feccb1324a4a63e9682f897c3714acb5e
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2023-01-06 17:49:01 -0300

    Revision/Correction + Comments

--- facpack_behavior/scripts/init.js
+++ facpack_behavior/scripts/init.js
@@ -1,12 +1,12 @@
 // Internal imports
-import { command_list } from './features/commands';
+import { COMMAND_LIST } from './features/commands';
 import { ChatFormatter } from './features/chat_formatter';
 
 // Initialize few variables
-let banned_from_broadcast_str = [];
+const banned_from_broadcast_str = [];
 
 /* CommandLine event listener */
-for (let cmd of command_list) {
+for (let cmd of COMMAND_LIST) {
     const executable = new cmd();
     banned_from_broadcast_str.push(executable.prefix + executable.command_id);
 }
