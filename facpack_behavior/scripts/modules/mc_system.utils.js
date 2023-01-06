c7c34615387d98ad11d354ce0723dff62b51657b
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2023-01-06 17:49:06 -0300

    Revision/Correction + Comments

--- facpack_behavior/scripts/modules/mc_system.utils.js
+++ facpack_behavior/scripts/modules/mc_system.utils.js
@@ -1,7 +1,12 @@
-import { world } from '@minecraft/server'
+import { world } from '@minecraft/server';
 
+/**
+ * This function adds an Event Before Chat.
+ * The callback arg is a functino that will run
+ * when the event occour.
+ */
 function eventBeforeChat(callback) {
     world.events.beforeChat.subscribe(callback);
 }
 
-export { eventBeforeChat }
+export { eventBeforeChat };
