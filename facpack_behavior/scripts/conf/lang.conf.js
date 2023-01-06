e034e09448769b9bd0c6e8fcfab3caafc67cbaa2
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2023-01-06 17:48:34 -0300

    Revision/Correction + Comments

--- facpack_behavior/scripts/conf/lang.conf.js
+++ facpack_behavior/scripts/conf/lang.conf.js
@@ -3,5 +3,6 @@ import { pt_br } from "./advices.texts";
 /**
  * Change the value and the import for change the
  * language of the advices about commandline system.
+ * Check ./advices.texts.js/ for more.
  */
 export const text = pt_br;
