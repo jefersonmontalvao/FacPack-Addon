96144bc79d5e4a6dda3d4cf34bf946d5afacfaa5
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2023-01-06 17:48:38 -0300

    Revision/Correction + Comments

--- facpack_behavior/scripts/features/chat_formatter.js
+++ facpack_behavior/scripts/features/chat_formatter.js
@@ -4,28 +4,36 @@ import { runMCCommandByEntity, getPlayerHierarchy } from "../modules/player_util
 
 class ChatFormatter {
     constructor(command_list) {
+        // Init few variables
         this.sender;
         this.message;
         this.tags = {
-            member: "§8Member§f",
-            builder: "§bBuilder§f",
-            admin: "§aAdmin§f",
-            owner: "§4Master§f"
+            guest: "§eGuest§r",
+            member: "§8Member§r",
+            builder: "§bBuilder§r",
+            admin: "§aAdmin§r",
+            owner: "§4Master§r"
         }
         this.message_template = '§8[§9%faction%§8][%tag%§8]§7%sender%§r§8> §7%message%';
 
-        eventBeforeChat(event => {
-            if (!this.chatEventIsCommand(event, command_list)) {
-                event.cancel = true;
+        // Initialize an event callback function
+        eventBeforeChat(chat => {
+            // Set value of few variables.
+            this.sender = chat.sender;
+            this.message = chat.message;
+
+            if (!this.chatEventIsCommandOfList(command_list)) {
+                /**
+                 * Cancel the message broadcast and send a formated message by tellraw mc command.
+                 */
+                chat.cancel = true;
                 runMCCommandByEntity(`tellraw @a {"rawtext": [{"text": "${this.formatTemplate(this.message_template)}"}]}`, this.sender);
             }
         });
     }
 
-    chatEventIsCommand(event_, command_list) {
-        this.sender = event_.sender;
-        this.message = event_.message;
-
+    /** Tests whether the sent message is a command */
+    chatEventIsCommandOfList(command_list) {
         for (let cmd of command_list) {
             if (cmd === this.message.split(' ')[0]) {
                 return true;
@@ -34,7 +42,12 @@ class ChatFormatter {
         return false;
     }
 
+    /**
+     * This function formats the message template and
+     * returns the formated template.
+     */
     formatTemplate(template) {
+        // Init few variables.
         let faction = (FactionHandler.playerHasFaction(this.sender) ? FactionHandler.getPlayerFaction(this.sender).id : '');
         let tag = this.tags[getPlayerHierarchy(this.sender)];
         let sender = this.sender.name;
