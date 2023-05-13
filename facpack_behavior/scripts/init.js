4dac7cf39799ff59a83570f78ca5d36a477badd3
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2023-05-13 13:07:39 -0300

    Removed world's mater variable.

--- facpack_behavior/scripts/init.js
+++ facpack_behavior/scripts/init.js
@@ -1,5 +1,6 @@
 // External imports
 import { world } from '@minecraft/server';
+import { getPlayerByName } from './modules/player_utils.js';
 
 // Internal imports
 import { COMMAND_LIST } from './features/commands';
@@ -10,12 +11,6 @@ import { text } from './conf/lang.conf';
 // Initialize few variables.
 let worlds_master = undefined;
 const banned_from_broadcast_str = [];
-
-// Set world's master value.
-if (worlds_master === undefined) {
-    worlds_master = world.getAllPlayers()[0];
-    sendAdviceToEntity(worlds_master, text.system.worldMasterInfo, 'info');
-}
 
 /* CommandLine event listener */
 for (let cmd of COMMAND_LIST) {
