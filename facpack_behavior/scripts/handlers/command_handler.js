3159efbf6d228dcf0daa51bc51d91d2c7534f7d1
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2023-05-13 21:52:38 -0300

    Added existent command validation.

--- facpack_behavior/scripts/handlers/command_handler.js
+++ facpack_behavior/scripts/handlers/command_handler.js
@@ -1,4 +1,8 @@
 import { eventBeforeChat } from '../modules/mc_system.utils';
+import { runMCCommandByEntity } from '../modules/player_utils';
+import { playSoundToPlayer } from '../modules/system.utils';
+import { tag_templates } from '../conf/advices.texts';
+import { text, text } from '../conf/lang.conf';
 
 /**
  * Thats the class that controls everything about 
@@ -11,7 +15,7 @@ class CommandHandler {
         this.prefix = prefix;
         this.command_id = id;
 
-        this.command; // command that was typed by sender.
+        this.playerCommandRequest; // command that was typed by sender.
         this.sender;
         this.target;
         this.full_command;
@@ -35,19 +39,49 @@ class CommandHandler {
             // Check if prefix was typed.
             if (chat.message.startsWith(this.prefix)) {
                 // Define the initial values of some variables.
-                this.command = chat.message
+                this.playerCommandRequest = chat.message
                     .slice(this.prefix.length)
                     .split(" ")[0];
 
                 this.sender = chat.sender;
                 this.full_command = chat.message;
                 
-                if (this.command === this.command_id) {
-                    chat.cancel = true; // do not broadcast
-                    this.commandCallback();
+                if (typeof this.command_id === 'string') {
+                    if (this.playerCommandRequest === this.command_id) {
+                        chat.cancel = true; // do not broadcast
+                        this.commandCallback();
+                    } else {
+                        sendNonExistentCommandAdvice();
+                    }
+                } else if (Array.isArray(this.command_id)) {
+                    // Dectect if the callback was called.
+                    let runned = false;
+                    for (id of this.command_id) {
+                        if (this.playerCommandRequest === id) {
+                            chat.cancel = true; // do not broadcast
+                            this.commandCallback();
+                            runned = true;
+                            break;
+                        }
+                    }
+                    if (!runned) {
+                        sendNonExistentCommandAdvice();
+                    }
                 }
             }
         })
+        
+        /**
+         * Send an advice about that command do not exists.
+         */
+        let sender = this.sender;
+        function sendNonExistentCommandAdvice() {
+            let text_template = text.system.non_existent_command
+                .replaceAll(tag_templates.system.player_command_request, sender);
+
+            runMCCommandByEntity(text_template, sender);
+            playSoundToPlayer(sender, 'error');
+        }
     }
 }
 
