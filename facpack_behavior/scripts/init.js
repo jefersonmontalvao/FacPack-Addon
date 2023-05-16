9ac4345af0289891cd866e1e2cd8c48458a237f7
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2023-05-15 21:53:37 -0300

    remove few imports that is not being used.

--- facpack_behavior/scripts/init.js
+++ facpack_behavior/scripts/init.js
@@ -1,15 +1,10 @@
 // External imports
-import { world } from '@minecraft/server';
-import { getPlayerByName } from './modules/player_utils.js';
 
 // Internal imports
 import { COMMAND_LIST } from './features/commands';
 import { ChatFormatter } from './features/chat_formatter';
-import { sendAdviceToEntity } from './modules/system.utils';
-import { text } from './conf/lang.conf';
 
 // Initialize few variables.
-let worlds_master = undefined;
 const banned_from_broadcast_str = [];
 
 /* CommandLine event listener */
